ab134cc154d3e8a89a6e43a6b2606ac344e5b14a
Thomas Bruderer <apophis@apophis.ch>
2019-11-28 23:51:15 +0100

    Automatically create an ino file * Should be easier to handle than the #ifdefs like in the compiles-for-arduino branch

---
+++ CreateIno/InoCreator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CreateIno
+{
+    internal class InoCreator
+    {
+        private readonly string _sourcePath;
+        private readonly List<string> _lines = new List<string>();
+
+        public InoCreator(string sourcePath)
+        {
+            _sourcePath = sourcePath;
+        }
+
+        public void AddFile(string relativeFilename, int from, int to)
+        {
+            foreach (var (line, index) in File.ReadAllLines(Path.Combine(_sourcePath, relativeFilename), Encoding.UTF8).Select((line, index) => (line, index)))
+            {
+                if (from <= index + 1 && index < to)
+                {
+                    _lines.Add(line);
+                }
+            }
+        }
+
+        public void AddLine(string line)
+        {
+            _lines.Add(line);
+        }
+
+        public void Save(string relativeFilename)
+        {
+            File.WriteAllLines(Path.Combine(_sourcePath, relativeFilename), _lines);
+        }
+    }
+}
---
+++ CreateIno/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CreateIno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InoCreator ino = new InoCreator(args.First());
+
+            ino.AddFile("AbstractLedStrip.h", 4, 17);
+
+            ino.Save("ChristmasLightsController.ino");
+        }
+    }
+}
